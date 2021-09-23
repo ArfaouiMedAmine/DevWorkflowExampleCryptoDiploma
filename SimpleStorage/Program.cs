7cada5e0c6add2acc88e5a81f864da6ad5e17f0a
Arfaoui Med Amine <mohamedamine.arfaoui@agilz.net>
2021-09-23 01:12:59 +0100

    Initial commit

---
+++ SimpleStorage/Program.cs
@@ -0,0 +1,57 @@
+﻿using Nethereum.Web3;
+using Nethereum.Web3.Accounts;
+using System;
+using System.Threading.Tasks;
+using SimpleStorage.SimpleStorage;
+using SimpleStorage.SimpleStorage.ContractDefinition;
+
+namespace SimpleStorage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo().Wait();
+        }
+
+        static async Task Demo()
+        {
+            try
+            {
+                // Setup
+                // Here we're using local chain eg Geth https://github.com/Nethereum/TestChains#geth
+                var url = "http://127.0.0.1:8545/";
+                var privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+                var account = new Account(privateKey);
+                var web3 = new Web3(account, url);
+
+                Console.WriteLine("Deploying...");
+                var deployment = new SimpleStorageDeployment();
+                var receipt = await SimpleStorageService.DeployContractAndWaitForReceiptAsync(web3, deployment);
+                var service = new SimpleStorageService(web3, receipt.ContractAddress);
+                Console.WriteLine($"Contract Deployment Tx Status: {receipt.Status.Value}");
+                Console.WriteLine($"Contract Address: {service.ContractHandler.ContractAddress}");
+                Console.WriteLine("");
+
+                Console.WriteLine("Sending a transaction to the function set()...");
+                var receiptForSetFunctionCall = await service.SetRequestAndWaitForReceiptAsync(
+                    new SetFunction() { X = 42, Gas = 400000 });
+                Console.WriteLine($"Finished storing an int: Tx Hash: {receiptForSetFunctionCall.TransactionHash}");
+                Console.WriteLine($"Finished storing an int: Tx Status: {receiptForSetFunctionCall.Status.Value}");
+                Console.WriteLine("");
+
+                Console.WriteLine("Calling the function get()...");
+                var intValueFromGetFunctionCall = await service.GetQueryAsync();
+                Console.WriteLine($"Int value: {intValueFromGetFunctionCall} (expecting value 42)");
+                Console.WriteLine("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine("Finished");
+            Console.ReadLine();
+        }
+    }
+}
